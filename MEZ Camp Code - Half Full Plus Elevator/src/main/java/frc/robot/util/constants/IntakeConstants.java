f5c0251ed0363d19d614766fc757db3b3f52a94a
navinphoebe <phoebe.c.navin@gmail.com>
2025-06-20 14:26:14 -0400

    Add another versiojn

---
+++ MEZ Camp Code - Half Full Plus Elevator/src/main/java/frc/robot/util/constants/IntakeConstants.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.util.constants;
+
+/** Add your docs here. */
+public class IntakeConstants {
+
+    //TODO: Find the actual value
+    public static final double MAX_HAS_CORAL_DISTANCE = .1;
+
+    public static final double REVERSE_SPEED = -0.2;
+    public static final double INTAKE_SPEED = 0.2;
+
+    public static final double JKG_METERS_SQUARED = 0.01;
+    public static final double GEAR_RATIO = 1;
+}
